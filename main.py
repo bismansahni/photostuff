db60279e074aee9c98d42aac78e803a8538a7d02
bismansahni <bismansahni@outlook.com>
2023-10-14 11:00:33 -0700

    added a working model

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template, session, redirect, url_for
 import hashlib
 
-app = Flask(__name)
+app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
 # Dummy database for user data
